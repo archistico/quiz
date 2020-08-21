544d522c0abe771da9281be5a134e37866dfe705
archistico <info@archistico.com>
2020-08-21 11:25:31 +0200

    sanitizza stringhe entrata

--- app/domande.php
+++ app/domande.php
@@ -11,6 +11,10 @@ class Domande
 
         $sql = "SELECT * FROM domande";
         $lista_domande = ($db->exec($sql));
+
+        array_walk_recursive($lista_domande, function(&$item) {
+            $item = htmlspecialchars_decode($item, ENT_QUOTES);
+        });
         
         $f3->set('domande', $lista_domande);
         $f3->set('titolo', 'Lista domande');
@@ -26,5 +30,29 @@ class Domande
         $f3->set('titolo', 'Nuova domanda');
         $f3->set('contenuto', 'domande/nuova.htm');
         echo \Template::instance()->render('templates/base.htm');
+    }
+
+    public function Registra($f3)
+    {
+        //FILTER_SANITIZE_ADD_SLASHES
+        $materia = filter_var(trim($f3->get('POST.materia')), FILTER_SANITIZE_STRING);
+        $domanda = filter_var(trim($f3->get('POST.domanda')), FILTER_SANITIZE_STRING);
+        $rispostaesatta = filter_var(trim($f3->get('POST.rispostaesatta')), FILTER_SANITIZE_STRING);
+        $rispostaerrata1 = filter_var(trim($f3->get('POST.rispostaerrata1')), FILTER_SANITIZE_STRING);
+        $rispostaerrata2 = filter_var(trim($f3->get('POST.rispostaerrata2')), FILTER_SANITIZE_STRING);
+        $rispostaerrata3 = filter_var(trim($f3->get('POST.rispostaerrata3')), FILTER_SANITIZE_STRING);
+        
+        $db = (\App\Db::getInstance())->connect();
+
+        $sql = "INSERT INTO domande VALUES (NULL, :domanda, :rispostaesatta, :rispostaerrata1, :rispostaerrata2, :rispostaerrata3);";
+        $db->exec($sql, [
+            ':domanda'=>$domanda,
+            ':rispostaesatta'=>$rispostaesatta,
+            ':rispostaerrata1'=>$rispostaerrata1,
+            ':rispostaerrata2'=>$rispostaerrata2,
+            ':rispostaerrata3'=>$rispostaerrata3
+            ]);
+        
+        $f3->reroute("@domande");
     }
 }
