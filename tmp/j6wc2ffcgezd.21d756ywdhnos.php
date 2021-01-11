9e166b86b082e3e5d121d41b90bacdded1813bfa
archistico <info@archistico.com>
2021-01-11 22:06:00 +0100

    remove tmp

--- tmp/j6wc2ffcgezd.21d756ywdhnos.php
+++
@@ -1,8 +0,0 @@
-<form action="/">
-    <p><?= ($domanda) ?>?</p>
-    <?php foreach (($risposte?:[]) as $risposta): ?>
-        <input type="radio" id="<?= ($risposta['id']) ?>" name="risposta" value="">
-        <label for="<?= ($risposta['id']) ?>"><?= ($risposta['testo']) ?></label><br>
-    <?php endforeach; ?>
-  </form>
-
--- tmp/j6wc2ffcgezd.3rid4e7el44k0.php
+++
@@ -1,1 +0,0 @@
-domande
